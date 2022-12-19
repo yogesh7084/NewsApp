117c6d0fd7832cc7e7d07327a8cd809a9140a469
Yogesh7084 <shingteyogesh7541@gmail.com>
2022-12-19 13:52:01 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <News/>
+    </>
+  );
+}
+
+export default App;
